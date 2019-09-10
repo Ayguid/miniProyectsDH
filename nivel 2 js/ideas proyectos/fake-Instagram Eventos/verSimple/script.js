f6e3575cac0316a62c4792c20df071732f57ce56
Ayguid <guidoaimar@gmail.com>
2019-09-10 01:22:12 -0300

    6to orden

---
+++ nivel 2 js/ideas proyectos/fake-Instagram Eventos/verSimple/script.js
@@ -0,0 +1,13 @@
+// var gallery = document.querySelector('.gallery');
+//
+// gallery.addEventListener('click', handleClick);
+//
+// function handleClick(event) {
+//   var target = event.target;
+//   target.classList.contains('like-container')?target.innerHTML=parseInt(target.innerHTML)+1:'';
+// }
+
+
+function likePost() {
+  event.target.innerHTML = parseInt(event.target.innerHTML)+1;
+}
