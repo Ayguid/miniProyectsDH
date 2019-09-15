c966b345924b30dfe714bb83ce8e3dde2102f2d4
Ayguid <guidoaimar@gmail.com>
2019-09-15 20:29:18 -0300

    13th commit

--- nivel 2 js/ideas proyectos/ver 1/fake-Instagram Eventos/verSimple/script.js
+++ nivel 2 js/ideas proyectos/ver 1/fake-Instagram Eventos/verSimple/script.js
@@ -2,9 +2,10 @@
 
 function likePost() {
   if (event.target.classList.contains('liked')) {
-    return;
+    event.target.classList.remove('liked');
+    event.target.innerHTML = " "+(parseInt(event.target.innerHTML)-1);
   }else {
     event.target.classList.add('liked');
-    event.target.innerHTML = parseInt(event.target.innerHTML)+1;//utilizamos este metodo "parseInt()" para pasar el "texto de html" a numeros y sumarle un like
+    event.target.innerHTML = " "+(parseInt(event.target.innerHTML)+1);//utilizamos este metodo "parseInt()" para pasar el "texto de html" a numeros y sumarle un like
   }
 }
